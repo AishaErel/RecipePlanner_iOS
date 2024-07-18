e66e523e69589446397e761fd026814dbed0bf7b
Aisha <aceydaerel11@gmail.com>
2024-07-17 18:38:06 -0700

    Initial Commit

---
+++ todolist/todolistApp.swift
@@ -0,0 +1,17 @@
+//
+//  todolistApp.swift
+//  todolist
+//
+//  Created by Ayse Ceyda Erel on 7/17/24.
+//
+
+import SwiftUI
+
+@main
+struct todolistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
